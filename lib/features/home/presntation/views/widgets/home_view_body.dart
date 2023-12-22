b43ce42a6085d8c699b97960896c6d576ebd44c0
Ammar74 <ammaryasser3016@gmail.com>
2023-12-22 02:07:32 +0200

    create syles file then add title

--- lib/features/home/presntation/views/widgets/home_view_body.dart
+++ lib/features/home/presntation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presntation/views/widgets/custom_app_bar.dart';
 import 'package:bookly/features/home/presntation/views/widgets/horizontal_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +13,17 @@ class HomeViewBody extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.only(top: 40.0, right: 18.0, left: 18.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomAppBar(image: AssetsData.logo, icon: Icons.search),
             HorizonatBookslListView(),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Best Sellers',
+                style: Styles.titleMedium,
+              ),
+            ),
           ],
         ),
       ),
