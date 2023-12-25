995e1d9b266357581f47293683e0763c17f87902
Ammar74 <ammaryasser3016@gmail.com>
2023-12-25 03:20:42 +0200

    create api service

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -4,5 +4,5 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchFeaturedbooks();
-  fetchBestSellerbooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerbooks();
 }
