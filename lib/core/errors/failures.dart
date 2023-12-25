10a7224387ee1d1540b45e494b385fa5bb65b74f
Ammar74 <ammaryasser3016@gmail.com>
2023-12-25 04:29:16 +0200

    create server failure from Dio error (handle all typees of errors)

--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,50 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection Timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send Timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive Timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Your Connection is Bad');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioError.response!.statusCode!, dioError.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request To Api Server was Cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Your Connection is Bad');
+      case DioExceptionType.unknown:
+        if (dioError.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please Try Again');
+      default:
+        return ServerFailure('Oops there was an Error, Please Try Again Later');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Page Not Found');
+    } else if (statusCode == 500) {
+      return ServerFailure('Server Error, Please Try Again Later');
+    } else {
+      return ServerFailure('Oops there was an Error, Please Try Again Later');
+    }
+  }
+}
